82e20ed2c546162e607aa9ee7a528e6aca7493a8
Natheesan30 <2020sp092@univ.jfn.ac.lk>
2024-12-15 14:31:35 +0530

    divide two number.

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -1,17 +1,9 @@
-// add two number and print
-void addTwoNumber(){
-  int numberOne = 3;
-  int numberTwo = 10;
-
-  // add two number
-int sum = numberOne + numberTwo;
-
-// print the number
-print(sum);
-
+import 'package:flutter/material.dart'; 
+void divide(int num1, int num2){
+  int result = num1 / num2;
+  print(result);
 }
-
 void main(){
-  //runapp();
-  addTwoNumber();
+  //runApp(); // Build the UI
+  divide(4,2);
 }
